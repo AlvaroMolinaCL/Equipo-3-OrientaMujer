e8ba18cd11b90dc40be621592e9fe8b2bfb8a287
MaicolRM88 <mramirezm@ing.ucsc.cl>
2025-03-29 23:13:18 -0300

    Actualización crud

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,7 @@
 <?php
-
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use IlluminateSupportFacadesRoute;
-use AppHttpControllersPostController;
+use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 // returns the home page with all posts
 Route::get('/', PostController::class .'@index')->name('posts.index');
