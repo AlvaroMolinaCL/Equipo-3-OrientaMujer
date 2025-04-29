6d66feb306f68fe082b6ccc0908707e6608ec8af
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2025-04-29 19:28:14 -0400

    Detalles en LP principal
    Se rellenan secciones faltantes de la landing page principal.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,12 +13,12 @@
         </a>
 
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link" href="/">Inicio</a></li>
-                <li class="nav-item"><a class="nav-link" href="/login">Usuari@</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Usuari@</a></li>
                 <li class="nav-item"><a class="nav-link" href="/servicios">Servicios</a></li>
                 <li class="nav-item"><a class="nav-link" href="/contacto">Contacto</a></li>
-                <li class="nav-item"><a class="nav-link" href="/tips">Tips</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Tips</a></li>
                 <li class="nav-item"><a class="nav-link" href="/acerca-de">Sobre Orienta Mujer</a></li>
                 <!--
                     <li class="nav-item" style="font-family: 'Courier Prime', Courier"><a class="nav-link" href="/">Inicio</a></li>
