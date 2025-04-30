e619df7fbb0ada69feddd5eae811860700e5bf8a
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2025-04-30 03:03:50 -0400

    Configuración inicial de Tenancy For Laravel
    Configuración inicial de Tenancy For Laravel para que, por ahora, pueda funcionar de forma local y se puedan hacer las pruebas necesarias antes de pasar al servidor. En Windows, se requiere usar XAMPP y agregar los subdominios creados en la BD en los virtual hosts de XAMPP (C:\xampp\apache\conf\extra\httpd-vhosts.conf), además, se debe modificar el archivo hosts agregando (con .localhost) los subdominios creados en la tabla tenants luego de crearlos en la BD (C:\Windows\System32\drivers\etc\hosts)

--- routes/tenant.php
+++ routes/tenant.php
@@ -3,35 +3,54 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
-use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
-use App\Http\Controllers\App\{ProfileController,UserController};
 
+/*
+|--------------------------------------------------------------------------
+| Tenant Routes
+|--------------------------------------------------------------------------
+|
+| Here you can register the tenant routes for your application.
+| These routes are loaded by the TenantRouteServiceProvider.
+|
+| Feel free to customize them however you want. Good luck!
+|
+*/
 
-Route::middleware([
-    'web',
-    InitializeTenancyByDomain::class,
-    PreventAccessFromCentralDomains::class,
-])->group(function () {
-
-    Route::get('/', function () {
-        return view('app.welcome');
-    });
+Route::middleware(['web'])->group(function () {
 
     Route::get('/dashboard', function () {
-        return view('app.dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
-    
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-        Route::resource('users', UserController::class);
-    
-    });
-    
-    
-    require __DIR__ . '/tenant-auth.php';
+        return view('dashboard');
+    })->middleware(['auth'])->name('dashboard');
 
+    // Página "Inicio"
+    Route::get('/', function () {
+        return view('index');
+    });
+
+    // Página "Servicios"
+    Route::get('/servicios', function () {
+        return view('services');
+    });
+
+    // Página "Contacto"
+    Route::get('/contacto', function () {
+        return view('contact');
+    });
+
+    // Página "Tips"
+    Route::get('/tips', function () {
+        return view('tips');
+    });
+
+    // Página "Sobre Orienta Mujer"
+    Route::get('/acerca-de', function () {
+        return view('about');
+    });
+
+    // Página para Agendar Asesorías
+    Route::get('/agenda', function () {
+        return view('agenda.index');
+    }); // Se debe agregar al middleware una vez se implemente el inicio de sesión
+
+    require __DIR__ . '/tenant-auth.php';
 });
