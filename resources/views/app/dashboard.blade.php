20d2af1d922896348a6e2ccceeee5aef2aff2020
MaicolRM88 <mramirezm@ing.ucsc.cl>
2025-04-30 02:01:36 -0400

    Implementación tenants & users

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
-                    <x-btn-link href="{{ route('tenants.index')}}">Tenants</x-btn-link>
+                    <x-btn-link href="{{ route('users.index')}}">Users</x-btn-link>
                 </div>
             </div>
         </div>
