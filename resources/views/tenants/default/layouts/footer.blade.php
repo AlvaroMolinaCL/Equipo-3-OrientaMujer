0e788cf403c96fca0dc981331c158c22562d28f4
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2025-05-11 01:26:48 -0400

    Se parametriza el footer

--- resources/views/tenants/default/layouts/footer.blade.php
+++ resources/views/tenants/default/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer id="site-footer" class="text-center py-4">
-    &copy; 2025 Orienta Mujer.
+    &copy; {{ date('Y') }} {{ tenantSetting('name', 'AbogaRed') }}. Desarrollado por AbogaRed.
 </footer>
