986f8098f742a78bf97bf6c3109bb0b8d421bc43
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2025-05-12 11:37:28 -0400

    Revert "esqueleto de paramterizacion de textos de la pagina about"
    This reverts commit 01f600a10eebbb0df475e7da3ac23e04396b0ea9.

--- app/helpers.php
+++ app/helpers.php
@@ -19,11 +19,8 @@ function tenantView(string $view): string
 /**
  * Recupera configuraciones de un tenant desde la base de datos
  */
-function tenantSetting($key, $default = null)
-{
-    $tenant = tenant(); // o como sea que accedas
-
-    return data_get($tenant->data ?? [], $key, $default);
+function tenantSetting($key, $default = null) {
+    return tenant()?->{$key} ?? $default;
 }
 
 /**
