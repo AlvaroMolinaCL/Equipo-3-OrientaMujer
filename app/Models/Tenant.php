ef34fa57aeef6871504478733ecc01ccf5497c25
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2025-05-12 11:37:28 -0400

    Revert "esqueleto de paramterizacion de textos de la pagina about"
    This reverts commit 8cbd286341ceb8c0975ee4ca8afa8572b2440019.

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -14,10 +14,7 @@ class Tenant extends BaseTenant implements TenantWithDatabase
     protected $primaryKey = 'id';
     public $incrementing = false;
     protected $keyType = 'string';
-    
-    protected $casts = [
-        'about' => 'array',
-    ];
+
     protected $fillable = [
         'id',
         'name',
@@ -38,7 +35,6 @@ class Tenant extends BaseTenant implements TenantWithDatabase
         'body_font',
         'link_font',
         'default_locale',
-        'about',
     ];
 
     protected static function booted()
