ef834ba484e2e4a8895513ea538e90d4c96e858f
MaicolRM88 <mramirezm@ing.ucsc.cl>
2025-05-18 02:34:40 -0400

    Al iniciar sesión, deja de aparecer login en navbar y muestra al user

--- resources/views/tenants/default/layouts/navigation.blade.php
+++ resources/views/tenants/default/layouts/navigation.blade.php
@@ -10,17 +10,23 @@
         </button>
 
         <a class="navbar-brand ms-auto" href="/">
-            <img src="{{ tenantSetting('logo_path_1', 'images/logo/Logo_1_(Predeterminado).png') }}" class="logo logo-1 d-none"
-                alt="Logo 1 {{ tenantSetting('name', 'Tenant') }}" width="300" height="50">
-            <img src="{{ tenantSetting('logo_path_2', 'images/logo/Logo_2_(Predeterminado).png') }}" class="logo logo-2 d-none"
-                alt="Logo 2 {{ tenantSetting('name', 'Tenant') }}" width="300" height="50">
+            <img src="{{ tenantSetting('logo_path_1', 'images/logo/Logo_1_(Predeterminado).png') }}"
+                class="logo logo-1 d-none" alt="Logo 1 {{ tenantSetting('name', 'Tenant') }}" width="300" height="50">
+            <img src="{{ tenantSetting('logo_path_2', 'images/logo/Logo_2_(Predeterminado).png') }}"
+                class="logo logo-2 d-none" alt="Logo 2 {{ tenantSetting('name', 'Tenant') }}" width="300" height="50">
         </a>
 
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link" href="/">Inicio</a></li>
+
                 @foreach ($enabledPages as $page)
                     @if ($page->is_visible)
+                        {{-- No mostrar login si está logueado --}}
+                        @if ($page->page_key === 'login' && Auth::check())
+                            @continue
+                        @endif
+
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url($page->page_key) }}">
                                 {{ $page->title ?? ucfirst($page->page_key) }}
@@ -28,7 +34,32 @@
                         </li>
                     @endif
                 @endforeach
+
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            @if(Auth::user()->hasRole('Admin'))
+                                <li><a class="dropdown-item" href="{{ route('dashboard') }}">Panel de administración</a></li>
+                            @endif
+                            <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Mi perfil</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item">Cerrar sesión</button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endauth
             </ul>
+
         </div>
     </div>
 </nav>
